5e3625aaff115a7c03eb07fb1380f5169823dc64
Brion Vibber <brion@pobox.com>
2014-09-04 13:38:09 -0700

    Initial Commit

---
+++ ModalTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ModalTest
+//
+//  Created by Brion on 9/4/14.
+//  Copyright (c) 2014 Brion Vibber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ModalTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ModalTest
+//
+//  Created by Brion on 9/4/14.
+//  Copyright (c) 2014 Brion Vibber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
